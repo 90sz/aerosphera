ed196d55d10d98d8733d0afb9cd69185794f258a
90s <delastoni1@gmail.com>
2026-07-31 02:11:48 +0200

    fix: secure Pages configuration and link Supabase

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,5 +2,3 @@ export const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL || '';
 export const SUPABASE_ANON_KEY = process.env.REACT_APP_SUPABASE_ANON_KEY || '';
 export const CLOUDINARY_CLOUD_NAME = process.env.REACT_APP_CLOUDINARY_CLOUD_NAME || 'dbaw8elnt';
 export const CLOUDINARY_UPLOAD_PRESET = process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET || 'vintage_media';
-export const CLOUDINARY_API_KEY = process.env.REACT_APP_CLOUDINARY_API_KEY || '542659795359421';
-export const CLOUDINARY_API_SECRET = process.env.REACT_APP_CLOUDINARY_API_SECRET || '7gTk_EqO3GLBuAeK6oUraStoQ74'; 
